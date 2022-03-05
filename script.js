1c988d934d1cee69b20ef9d4b21fc1eb716df8bf
Vinay G Shet <80429916+vinaygshet@users.noreply.github.com>
2022-03-05 17:14:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+//random practice questions
+//question1
+let gifts = ["teddy bear", "drone", "doll"];
+for (let i = 0; i < gifts.length; i++) {
+  console.log(`Wrapped ${gifts[i]} and added a bow!`);
+}
+
+//q2
+var lemein = "0";
+var lemeout = 0;
+var msg = "";
+if (lemein) {
+ msg += "hi";
+ }
+if (lemeout) {
+ msg += "Hello";
+}
+console.log(msg);
+//q3
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var new_string = "";
+ 
+for (var i = 0; i < 11; i++) {
+ new_string += numsArr[i] 
+}
+console.log(new_string);
+//q4
+
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+for (var i = 0; i <=10; i++) {
+ if(numsArr[i] %2 == 0 )
+ {
+ numsArr[i] = "even"
+ }
+
+}
+console.log(numsArr);
+//q5
+
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var sum=0;
+for (var i = 0; i <10; i++) {
+ if(numsArr[i]%2==0);
+ sum += numsArr[i]
+}
+console.log(sum);
